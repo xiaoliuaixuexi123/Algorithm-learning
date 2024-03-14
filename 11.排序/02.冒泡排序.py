7e2be292c3d4cdb8e2fb53d74e6dbadcf9dc4ffb
xiaoliuaixuexi123 <1608638993@qq.com>
2024-03-14 13:10:14 +0800

    02.冒泡排序

---
+++ 11.排序/02.冒泡排序.py
@@ -0,0 +1,20 @@
+from random import randint
+
+
+def bubble_sort(nums: list[int]):
+    """冒泡排序"""
+    n = len(nums)
+    # 外循环：未排序区间为 [0, i]
+    for i in range(n - 1, 0, -1):
+        # 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
+        for j in range(i):
+            if nums[j] > nums[j + 1]:
+                # 交换 nums[j] 与 nums[j + 1]
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+
+if __name__ == '__main__':
+    random_list = [randint(0,100) for _ in range(100)]
+    print(random_list)
+    bubble_sort(random_list)
+    print(random_list)
